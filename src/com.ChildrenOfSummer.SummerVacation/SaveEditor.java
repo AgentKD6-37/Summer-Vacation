31483b5640f5e4b577734ebf6a2d9e29f00679eb
Michael  Stack <mstack000@gmail.com>
2022-01-26 18:31:29 -0500

    Created Start Menu and selection and started building the character creator for the player character. This effort is going toward AB#182 and task AB#242

--- src/com.ChildrenOfSummer.SummerVacation/SaveEditor.java
+++ src/com.ChildrenOfSummer.SummerVacation/SaveEditor.java
@@ -1,5 +1,8 @@
 package com.ChildrenOfSummer.SummerVacation;
 
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,74 +20,45 @@ public class SaveEditor {
     private FileWriter writer;
     private Scanner scanner = new Scanner(System.in);
     private FileReader reader;
-    private String saveGame = "Assets/save-game.properties";
+    private static String save = "Assets/save-game.properties";
     public Properties saveFile;
 
     //returns the save file to the game engine to be read
-    public Object getFlag(Properties flag) throws IOException {
-        saveFile = new Properties();
-        in = new FileInputStream(saveGame);
-        saveFile.load(in);
-        return saveFile.get(flag);
-    }
 
-    public void getAssetFile (String fileName) throws IOException{
+    public static void getAssetFile(String fileName) throws IOException{
         String art = "Assets/" + fileName;
         var out = new BufferedOutputStream(System.out);
         Files.copy(Path.of(art), out);
         out.flush();
     }
 
-    void newPlayerCreator() throws IOException{
-        saveFile = new Properties();
-        in = new FileInputStream(saveGame);
-        saveFile.load(in);
-        in.close();
-        out = new FileOutputStream(saveGame);
-        System.out.print("What is your name? : ");
-        System.out.println();
-        String name = scanner.next();
-        saveFile.put("playerName", name);
-        saveFile.put("rlglComplete", Boolean.toString(false));
-        saveFile.put("isAlive", Boolean.toString(true));
-        System.out.print("Have you played this game before? [Y/N] : ");
-        System.out.println();
-        String newPlayer = scanner.next();
-        if (newPlayer.equals("N")) {
-            saveFile.put("newPlayer", Boolean.toString(true));
-            getAssetFile("new-player-story.txt");
-        }else if (newPlayer.equals("Y")){
-            System.out.println("starting game");
-        } else {
-            System.out.println("Enter only Y or N");
-            System.out.println();
-            newPlayer = scanner.next();
-        }
-        saveFile.store(out, "Player Save File");
-        out.close();
-    }
-
     /*
      * These methods load sets of asset files for different menus.
      */
 
-    public void MenuFiles() throws IOException {
-
-        getAssetFile("opening-menu-art.txt");
-        getAssetFile("opening-menu-banner.txt");
-        getAssetFile("opening-menu-dialogue.txt");
+    public static void menuFiles() {
+        try {
+            getAssetFile("opening-menu.txt");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     //save-game.properties is set by default to null/0 values and then overwritten by the newPlayerCreator
 
-    public Properties getSaveFile() throws IOException {
-        try {
-            saveFile = new Properties();
-            in = new FileInputStream(saveGame);
-            saveFile.load(in);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }return saveFile;
+    public static Object getSaveFile() {
+        Object jsonSave = null;
+        try{
+            //create JSON Parser and file reader then create a JSON reader by combining them
+            JSONParser jsonParser = new JSONParser();
+            FileReader fileReader = new FileReader(save);
+            jsonSave = jsonParser.parse(fileReader);
+            System.out.println(jsonSave);
+
+        }catch(IOException|ParseException e) {
+            System.err.print("No save file present! Please create a new game.");
+        }
+        return jsonSave;
     }
 
 }
