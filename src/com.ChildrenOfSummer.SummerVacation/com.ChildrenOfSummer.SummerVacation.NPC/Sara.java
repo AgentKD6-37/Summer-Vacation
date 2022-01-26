82408ffd79100318876efa1ceb572f579b007e96
Michael  Stack <mstack000@gmail.com>
2022-01-26 16:05:32 -0500

    Adding NPCS, AB#79

--- src/com.ChildrenOfSummer.SummerVacation/com.ChildrenOfSummer.SummerVacation.NPC/Sara.java
+++
@@ -1,7 +0,0 @@
-package com.ChildrenOfSummer.SummerVacation.com.ChildrenOfSummer.SummerVacation.NPC;
-
-public class Sara extends Friend{
-    public Sara(String name) {
-        super(name);
-    }
-}
