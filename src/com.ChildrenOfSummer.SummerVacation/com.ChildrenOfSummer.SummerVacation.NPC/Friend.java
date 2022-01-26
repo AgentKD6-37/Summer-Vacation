12796b8d0d38af4a741160a07f46a72ad706c4fb
Michael  Stack <mstack000@gmail.com>
2022-01-26 13:02:16 -0500

    save editor added for Fan to tool with for Engine Development

---
+++ src/com.ChildrenOfSummer.SummerVacation/com.ChildrenOfSummer.SummerVacation.NPC/Friend.java
@@ -0,0 +1,7 @@
+package com.ChildrenOfSummer.SummerVacation.com.ChildrenOfSummer.SummerVacation.NPC;
+
+public class Friend extends NPC{
+    public Friend(String name) {
+        super(name);
+    }
+}
