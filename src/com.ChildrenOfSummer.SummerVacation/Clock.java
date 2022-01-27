f38f84f7572189a778701d48ca406ffae38a8bb0
Fan <fanwu513@gmail.com>
2022-01-27 15:37:19 -0500

    added player and clock classes
    added player and clock classes, AB#301

---
+++ src/com.ChildrenOfSummer.SummerVacation/Clock.java
@@ -0,0 +1,59 @@
+package com.ChildrenOfSummer.SummerVacation;
+
+import java.util.Calendar;
+import java.util.Date;
+
+class Clock {
+
+    private static int NEXTDAYCOUNTER;
+    private static int THREEHOURCOUNTER;
+    static int daysCounter = 42;
+
+    static void incrementNextDay(){
+        Date date = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.DATE, 1);
+        date = c.getTime();
+        System.out.println(date);
+        daysCounter++;
+    }
+
+    void incrementByThreeHours(){
+        Date date = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.HOUR_OF_DAY, 3);
+        date = c.getTime();
+        System.out.println(date);
+        THREEHOURCOUNTER++;
+    }
+
+    static void wakeUpTime(){
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.HOUR_OF_DAY, 7);
+        calendar.add(Calendar.DATE,1);
+        System.out.println(calendar.getTime());
+    }
+
+   void today(){
+        Date date = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        System.out.println(date);
+    }
+
+    void timeTravel(){
+        if (THREEHOURCOUNTER == 5){
+            daysCounter++;
+            incrementNextDay();
+        }
+    }
+
+
+}
---
+++ src/com.ChildrenOfSummer.SummerVacation/Player.java
@@ -0,0 +1,35 @@
+package com.ChildrenOfSummer.SummerVacation;
+
+class Player {
+    void sleep(){
+        Clock.incrementNextDay();
+    }
+
+    void move(){
+
+    }
+
+    void wakeUp(){
+        Clock.wakeUpTime();
+    }
+
+    void talk(){
+
+    }
+
+    void shop(){
+
+    }
+
+    void get(){
+
+    }
+
+    void drop(){
+
+    }
+
+    void combine(){
+
+    }
+}
